20ce6666193ab871c39b0d18db38754a73c1af80
Vansh <saini40vansh@gmail.com>
2026-08-14 00:26:49 +0530

    Fix GitHub Pages deployment

--- jwt-auth-frontend/jwt-auth-frontend/vite.config.js
+++ jwt-auth-frontend/jwt-auth-frontend/vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/JWT-Auth-Demo/",
+
   plugins: [react()],
+
   server: {
     port: 5173,
     proxy: {
